7ae7244ee738200ec8a7bb71c0c5173d25c219bb
Wes Dean <wdean@flexion.us>
2024-04-29 13:09:48 -0400

    Refactor and document code

--- sync.py
+++ sync.py
@@ -1,55 +1,126 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
+"""
+@file sync
+@brief Synchronize a GitHub organization's membership to a Team
+@details
+GitHub does not create a team that automatically includes all of
+the organization's members.  Because collaborators are either
+individuals or teams but not organizations, there is no native
+way to grant all members of an organization access to, for example,
+an internal repository.
+
+Therefore, this script will create and populate a team in an
+organization that has as its membership the members of the
+organization.  This allows folks to provide access to a team
+that represents everyone in the organization.  For example:
+
+    @my-organization/everyone
+"""
+
+import logging
 import os
 
 from github import Github
 from github import Auth
 from dotenv import load_dotenv
 
-load_dotenv()
 
-PAT = os.environ['PAT']
-ORG = os.environ['ORG']
-TEAM_NAME = os.environ['TEAM_NAME']
+def create_team_if_not_exists(team_name, organization):
+    """
+    @fn create_team_if_not_exists()
+    @brief if a team doesn't exist, create it; either way, return it
+    @details
+    Given an organization and the name of a team, check to see if a
+    team with that name exists in the organization.  If it exists,
+    return a team object immediately; if it does not exist, create
+    the team (requires admin:org scope) and then return a team object
+    @param team_name the name of the team (just the name, not the org)
+    @param organization the organization object
+    @returns team object
+    @par Example
+    @code
+    my_team = create_team_if_not_exists("my_team", my_org)
+    @endcode
+    """
 
-auth = Auth.Token(PAT)
+    logging.debug("Fetching teams from organization")
+    teams = organization.get_teams()
 
+    for team in teams:
+        logging.debug(
+            'Comparing requested "%s" with detected "%s"', team_name, team.name
+        )
+        if team.name == team_name:
+            logging.debug("Found a match")
+            return team
 
-current_org_members = {}
-current_team_members = {}
-
-g = Github(auth=auth)
-
-organization = g.get_organization(ORG)
-org_members = organization.get_members()
-teams = organization.get_teams()
-
-team_exists = False
-
-for team in teams:
-    if team.name == TEAM_NAME:
-        the_team = team
-        team_exists = True
-
-if not team_exists:
-    print(f"team {TEAM_NAME} doesn't exist so creating it")
-    the_team = organization.create_team(TEAM_NAME)
-else:
-    print(f"team {TEAM_NAME} already exists")
-
-team_members = the_team.get_members()
-
-for member in team_members:
-    current_team_members[member.login] = True
-
-for member in org_members:
-    current_org_members[member.login] = True
+    logging.info('Team "%s" was not found; creating it.', team_name)
+    return organization.create_team(team_name)
 
 
-for member in current_org_members.keys():
-    if not current_team_members[member]:
-        print(f"{member} is in the org but not the team, so adding them")
-        the_team.add_membership(member)
-    else:
-        print(f"{member} is in the org and in the team")
+def get_group_logins(group):
+    """
+    @fn get_group_logins()
+    @brief given a team or an organization, return a dict of its members
+    @details
+    This will iterate through the list of members in either a team or
+    an organization and return a dict with each member's login as the
+    key and True as the value.
+    @param group the team or organization object to query
+    @returns dict with the members' logins as keys
+    @par Examples
+    @code
+    team_members = get_group_logins(team)
+    org_members = get_group_logins(organization)
+    @endcode
+    """
 
+    member_logins = {}
+
+    for member in group.get_members():
+        logging.debug('Found member "%s"', member.login)
+        member_logins[member.login] = True
+
+    return member_logins
+
+
+def main():
+    """
+    @fn main()
+    @brief the main function
+    """
+
+    # logging.basicConfig(level=logging.DEBUG)
+
+    load_dotenv()
+
+    pat = os.environ["PAT"]
+    org = os.environ["ORG"]
+    team_name = os.environ["TEAM_NAME"]
+
+    logging.debug('Using PAT "%s**************************"', pat[:8])
+    logging.debug('Using ORG "%s"', org)
+    logging.debug('Using TEAM_NAME "%s"', team_name)
+
+    auth = Auth.Token(pat)
+
+    g = Github(auth=auth)
+
+    organization = g.get_organization(org)
+
+    team = create_team_if_not_exists(team_name, organization)
+
+    current_org_members = get_group_logins(organization)
+    current_team_members = get_group_logins(team)
+
+    for member in current_org_members:
+        if not current_team_members[member]:
+            logging.info('"%s" is in the org but not the team, so adding them', member)
+            team.add_membership(member)
+        else:
+            logging.debug('"%s" is in the org and in the team', member)
+
+
+if __name__ == "__main__":
+    main()
